c04213fd9231ec9807b3c9124d32ba48d7c2136d
HenniePenny <h.kressner@hotmail.de>
2022-10-30 18:54:08 +0100

    added resolvers for query and mutations

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [BookType]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type BookType {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  input Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    saveBook(body: Book!): User
+    deleteBook(bookId: String): User
+  }
+`;
+
+module.exports = typeDefs;
